ddc24bb68b3476ea681bf772760d50f6cf572bd2
Tomal Hossain <tomalhossain@gmail.com>
2015-11-23 14:33:03 -0500

    Successfully compiled ShortestPathsMain.java which thus far has only established the GridMaster data structure. Next step is to get rid of run time exceptions for the structure. Then, begin writing actual Dijkstra's algorithm code.

--- ShortestPathsMain.java
+++ ShortestPathsMain.java
@@ -1,43 +1,52 @@
-import java.util.Map;
+/*import java.util.Map;
 import java.util.AbstractMap; 
 import java.util.HashMap;
 import java.util.List;
-import java.util.ArrayList;
-import java.io.BufferedReader;
+import java.util.ArrayList;*/
+
+/*import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.InputStreamReader;*/
+import java.io.*;
 import java.lang.*; 
+import java.util.*;
 
+/*
+- The overall data type that will house the entire list of vertices. 
+- The outermost level of the mastergrid is a 2-D Arraylist which represents the matrix of cells
+each of which contains k vertices. 
+- Within each cell is a HashMap which maps a given vertex in the cell to its adjecent vertices. 
+- The adjacent vertices are stored in an ArrayList, and each element in this Arraylist is a 
+pair of values (AbstractMap.SimpleEntry), namely the name of the adjacent vertext and the 
+weight to that vertex. 
+
+d[v] // distance of v from src - initialize to MAX.VALUE
+p[v] // parent of v in shortest path - initialize to nil
+d[s] = 0  
+
+// S set of all nodes that already have found shortest path
+// Q all verts in graph
+
+@author Tomal Hossain
+@version 0.1 2015-11-23
+
+*/
 
 public class ShortestPathsMain {
 	
-	/*
-	- The overall data type that will house the entire list of vertices. 
-	- The outermost level of the mastergrid is a 2-D Arraylist which represents the matrix of cells
-	each of which contains k vertices. 
-	- Within each cell is a HashMap which maps a given vertex in the cell to its adjecent vertices. 
-	- The adjacent vertices are stored in an ArrayList, and each element in this Arraylist is a 
-	pair of values (AbstractMap.SimpleEntry), namely the name of the adjacent vertext and the 
-	weight to that vertex. 
 
-	d[v] // distance of v from src - initialize to MAX.VALUE
-	p[v] // parent of v in shortest path - initialize to nil
-	d[s] = 0  
-
-	// S set of all nodes that already have found shortest path
-	// Q all verts in graph
-
-	*/
 	ArrayList<ArrayList<HashMap<String, ArrayList<AbstractMap.SimpleEntry<String, Integer>>>>> masterGrid; 
 
-	public recalculate (int u, int v, int w) {
+/*
+	public void recalculate (int u, int v, int w) {
 
 		if (d[v] > d[u] + w (u,v)) {
 			d[v] = d[u]  + w (u,v);
 			p[v] = u; 
 		}
 	}
+*/
 
 	public int getWeight (int i, int j, String hashKey, int adjVertIndex) {
 
@@ -47,15 +56,16 @@ public class ShortestPathsMain {
 
 	public void addAdjVert (int i, int j, String hashKey, String adjVertex, int weight) {
 
-			Integer weightInteger = new Integer (weight); 
-			Map.Entry<String, Integer> pair = new AbstractMap.SimpleEntry<String, Integer>(adjVertex, weightInteger); 
-			masterGrid.get(i).get(j).get(hashKey).add(pair);
-		}
+		Integer weightInteger = new Integer (weight); 
+		AbstractMap.SimpleEntry<String, Integer> pair = new AbstractMap.SimpleEntry<String, Integer>(adjVertex, weightInteger); 
+		masterGrid.get(i).get(j).get(hashKey).add(pair);
 	}
+	
 
 	public void addVertToCell (int i, int j, String hashKey) {
 
-		ArrayList adj = new ArrayList(); 
+		ArrayList adj = new ArrayList();
+		System.out.println(Integer.toString(i) + " " + Integer.toString(j));  
 		masterGrid.get(i).get(j).put(hashKey, adj); 
 
 	}
@@ -68,61 +78,70 @@ public class ShortestPathsMain {
 		String widthIndex = String.valueOf(w); 
 		String heightIndex = String.valueOf(h);
 		String numVerts = String.valueOf(k);
+		String line; 
 
-		while ((line = br.readLine()) != null) {
+		try {
+  			while ((line = br.readLine()) != null && line != "queries") {
 			
-			String[] spliced = line.split("\\s+");
-			String[] internal = spliced[0].split(".");
+				String[] spliced = line.split("\\s+");
+				String vert1 = spliced[0];
+				String vert2 = spliced[1];
+				String edge = spliced[2];
+				int edgeWeight = Integer.parseInt(edge); 
 
-			if (internal.length == 2) {
+				String[] internal = spliced[0].split(".");
+				String heightChar =  Character.toString(internal[0].charAt(1));
+				String widthChar = internal[1];
+				String vertNum = internal[2];  
 
-				String heightChar = internal[0].charAt(1);
-				String widthChar = internal[1]; 
 
-				addGridCorner(heightChar, widthChar, spliced);
+				int heightCharIndex = Integer.parseInt(heightChar);
+				int widthCharIndex = Integer.parseInt(widthChar); 
 				
-				if (internal[0].charAt(1) == "0") { 
-					if (internal[1]  == "0") {
-						addAdjVert(0, 0, spliced[0], spliced[1], spliced[2]); 	 
+				if (internal.length == 2) {
+					if (heightChar == "0") { 
+						if (widthChar  == "0") {
+							addAdjVert(0, 0, vert1, vert2, edgeWeight); 	 
+						}
+						else if (widthChar  == widthIndex) {
+							addAdjVert(0, w, vert1, vert2, edgeWeight); 	 
+						}
+						else {
+							addAdjVert(0, widthCharIndex-1, vert1, vert2, edgeWeight);
+							addAdjVert(0, widthCharIndex, vert1, vert2, edgeWeight); 
+						}	 
 					}
-					else if (internal[1]  == widthIndex) {
-						addAdjVert(0, w, spliced[0], spliced[1], spliced[2]); 
+					else if (heightChar == heightIndex) { 
+						if (widthChar  == "0") {
+							addAdjVert(h, 0, vert1, vert2, edgeWeight); 	 
+						}
+						else if (widthChar  == widthIndex) {
+							addAdjVert(h, w, vert1, vert2, edgeWeight); 
+						}
+						else {
+							addAdjVert(h, widthCharIndex-1, vert1, vert2, edgeWeight);
+							addAdjVert(h, widthCharIndex, vert1, vert2, edgeWeight); 
+						}	 
 					}
-					else {
-						int innerWidthIndex = Integer.parseInt(internal[1]);
-						addAdjVert(0, innerWidthIndex-1, spliced[0], spliced[1], spliced[2]);
-						addAdjVert(0, innerWidthIndex, spliced[0], spliced[1], spliced[2]); 
-					}	 
+					else if (widthChar  == "0") {
+						addAdjVert(heightCharIndex, 0, vert1, vert2, edgeWeight);
+						addAdjVert(heightCharIndex-1, 0, vert1, vert2, edgeWeight); 
+					}
+					else if (widthChar  == widthIndex) {
+						addAdjVert(heightCharIndex, w, vert1, vert2, edgeWeight);
+						addAdjVert(heightCharIndex-1, w, vert1, vert2, edgeWeight); 
+					}
+				}
+
+				else {
+					addAdjVert (heightCharIndex, widthCharIndex, vert1, vert2, edgeWeight);
 				}
 			}
 		}
-
-	public void addGridCorner (String heightChar, String widthChar, String[] spliced, String widthIndex, String heightIndex) {
-
-		if (internal[0].charAt(1) == heightIndex) { 
-			if (internal[1]  == "0") {
-				addAdjVert(0, 0, spliced[0], spliced[1], spliced[2]); 	 
-			}
-			else if (internal[1]  == widthIndex) {
-				addAdjVert(0, w, spliced[0], spliced[1], spliced[2]); 
-			}
-			else {
-				int innerWidthIndex = Integer.parseInt(internal[1]);
-				addAdjVert(0, innerWidthIndex-1, spliced[0], spliced[1], spliced[2]);
-				addAdjVert(0, innerWidthIndex, spliced[0], spliced[1], spliced[2]); 
-			}	 
-		}
+		catch (IOException e) {
+	    	System.out.println("Buffered reader error"); 
+	    }
 	}
-
-		/*
-	public void Djikstra () {
-		extract min from Q
-		add to S
-
-		for each v adj to u
-			relax (u, v, w); 
-	}
-	*/
 
 	public void initializeGrid (int w, int h, int k) {
 
@@ -132,44 +151,79 @@ public class ShortestPathsMain {
 				for (int di = 0; di <= 1; di++) {
 					for (int dj = 0; dj <= 1; dj++) {
 					    String hashKey1 = "g" + Integer.toString(i + di) + "." + Integer.toString(j + dj); 
-						masterGrid.addVertToCell(i, j, hashKey1);
+						addVertToCell(i, j, hashKey1);
 					    }
 				    }
 				// make the rest
 				for (int v = 0; v < k; v++) {
 					String hashKey2 = "v" + i + "." + j + "." + v; 
-					masterGrid.addVertToCell(i, j, hashKey2);
+					addVertToCell(i, j, hashKey2);
 				}
+			}
+		}
+	}
+
+	public static void printMap (HashMap<String, ArrayList<AbstractMap.SimpleEntry<String, Integer>>> map) {
+	 Iterator it = map.entrySet().iterator();
+		while (it.hasNext()) {
+		    Map.Entry <String, ArrayList<AbstractMap.SimpleEntry<String, Integer>>> pair = (Map.Entry<String, ArrayList<AbstractMap.SimpleEntry<String, Integer>>>)it.next();
+		    System.out.print(pair.getKey() + " --->" ); 
+		    ArrayList <AbstractMap.SimpleEntry<String, Integer>> adjVerts = pair.getValue();
+		    for (int i = 0; i < adjVerts.size(); i++) {
+		    	System.out.print(" (" + adjVerts.get(i).getKey() + ", " + adjVerts.get(i).getValue() + ")");
+		    }
+		    System.out.println(); 
+		    it.remove(); // avoids a ConcurrentModificationException
+		}
+	}
+
+	public void printMasterGrid (ArrayList<ArrayList<HashMap<String, ArrayList<AbstractMap.SimpleEntry<String, Integer>>>>> masterGrid, int h, int w, int k) {
+	
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				printMap(masterGrid.get(i).get(j)); 
 			}
 		}
 	}
 
 	public static void main (String args[]) {
 
+		ShortestPathsMain shortestMain = new ShortestPathsMain(); 
+
 		if (args.length == 0) {
-			System.err.println("USAGE: specify a MakeRandomGraph input file
-				as the first command line argument");
+			System.err.println("USAGE: specify a graph input file as the first command line argument");
 			System.exit(1);  
 		}
 
-		//ShortestPathsMain shortest = new ShortestPathsMain(); 
+		FileReader reader = null;
+		try {
+			reader = new FileReader(args[0]); 	
+		} 
+		catch (FileNotFoundException e)
+    	{
+      		System.err.println("File does not exit. Program will now terminate.");
+      		System.exit(0);
+    	}
 
-		try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
-	    String line;
+		BufferedReader br = new BufferedReader(reader);
+	    String line = "";
 
-	    line = br.readLine()
+	    try {
+	    	line = br.readLine();
+	    }
+	    catch (IOException e) {
+	    	System.out.println("Buffered reader error"); 
+	    }
+
 	    String[] spliced = line.split("\\s+"); 
 
 	    int w = Integer.parseInt(spliced[0]);
 	    int h = Integer.parseInt (spliced[1]);  
 	    int k = Integer.parseInt(spliced[1]); 
 
-	    masterGrid = new ArrayList<ArrayList<HashMap<String, ArrayList<AbstractMap.SimpleEntry>>(k)>(h)>(w); 
-	    initializeGrid(w, h, k); 
-
-	    addWeights(br, width, height, numVerts); 
-
-		    
-		}
+	    shortestMain.masterGrid = new ArrayList<ArrayList<HashMap<String, ArrayList<AbstractMap.SimpleEntry<String, Integer>>>>>(); 
+	    shortestMain.initializeGrid(w, h, k); 
+	    shortestMain.addWeights(br, w, h, k); 
+	    shortestMain.printMasterGrid(shortestMain.masterGrid, h, w, k);
 	}
 }
